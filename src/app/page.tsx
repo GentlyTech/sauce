b1c33dbd92875e457cb05470680670eea0c50e87
GentlyTech <andyyep@hotmail.com>
2024-03-30 15:20:52 -0400

    Fix card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
+import CustomCard from "@/components/custom-card";
 import Search from "@/components/search";
-import {Button, Flex, Layout} from "antd";
+import { Button, Flex, Layout } from "antd";
 
 export default function Home() {
+  return (
+    <div className="w-100 h-100">
+      <Flex
+        justify={"space-between"}
+        align={"center"} /*style={{backgroundColor: 'lightblue'}}*/
+      >
+        <h1>Hotels</h1>
+        <Button type={"default"} size={"large"} href={"/login"}>
+          Login
+        </Button>
+      </Flex>
 
-    return (
-        <>
-            <Flex justify={'space-between'} align={'center'} /*style={{backgroundColor: 'lightblue'}}*/>
-                <h1>Hotels</h1>
-                <Button type={'default'} size={'large'} href={'/login'}>Login</Button>
-            </Flex>
+      <div className="flex flex-col items-center">
+        <Search />
+      </div>
 
-            <Search/>
-        </>
-
-    );
+      <div className="flex flex-row gap-2">
+        <CustomCard title="SomeHotel" body="Blah blah" img="" />
+        <CustomCard title="SomeHotel" body="Blah blah" img="" />
+        <CustomCard title="SomeHotel" body="Blah blah" img="" />
+      </div>
+    </div>
+  );
 }
--- src/components/custom-card.tsx
+++ src/components/custom-card.tsx
@@ -1,18 +1,19 @@
 import { Card } from "antd";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function CustomCard(props: CardProps) {
   return (
     <Card className="flex flex-col mw-5">
-      <Image className="w-100" alt="Preview of a hotel or hotel room" src={props.img ? props.img : ""} />
-      <h2>{props.title ? props.title : "Title"}</h2>
-      <caption>{props.body ? props.body : "Body"}</caption>
+      <Image className="w-100" alt="Preview of a hotel or hotel room" src={props.img} />
+      <h2 className="w-100">{props.title}</h2>
+      <p className="w-100">{props.body}</p>
     </Card>
   );
 }
 
 interface CardProps {
-  title?: string;
-  body?: string;
-  img?: string;
+  title: string;
+  body: string;
+  img: string;
 }
