3016d0a0e24fbf56ab86d9003fdd13ec6c5eb9f9
GentlyTech <andyyep@hotmail.com>
2024-03-31 01:40:25 -0400

    Add error handling on fetch failures Reformat files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,42 @@
 import CustomCard from "@/components/custom-card";
 import Search from "@/components/search";
-import {Button, Flex, Layout} from "antd";
-import {BaseOptionType} from "rc-select/es/Select";
-import getByHotelChains, {getHotelLocations} from "@/lib/actions";
-import {getHotelChains} from "@/lib/actions";
-import {GetAllHotels} from "@/lib/data";
-
+import { Button, Flex } from "antd";
+import { getAllHotels, getHotelLocations } from "@/lib/actions";
 
 export default async function Home() {
-    const hotels = await GetAllHotels();
+  const hotels = await getAllHotels();
+  let locations = await getHotelLocations();
 
-    const hotelCards = hotels?.map((hotel, index) => {
-        return <CustomCard key={hotel.hotelId} title={hotel.hotelName} subtitle=""
-                           body={`${hotel.address.street}, ${hotel.address.city}, ${hotel.address.province} ${hotel.address.postalCode}, ${hotel.address.country}`}
-                           img=""/>;
-    });
-
+  const hotelCards = hotels?.map((hotel, index) => {
     return (
-        <div className="flex flex-col w-full h-full p-2 overflow-hidden box-border">
-            <Flex
-                justify={"space-between"}
-                align={"center"} /*style={{backgroundColor: 'lightblue'}}*/
-            >
-                <h1>Hotels</h1>
-                <Button type={"default"} size={"large"} href={"/login"}>
-                    Login
-                </Button>
-            </Flex>
-            <div className="flex flex-col items-center">
-                <Search locations={await getHotelLocations()}/>
-            </div>
-            <div className="flex flex-row flex-wrap gap-2 overflow-y-auto box-border">{hotelCards}</div>
-        </div>
+      <CustomCard
+        key={hotel.hotelId}
+        title={hotel.hotelName}
+        subtitle=""
+        body={`${hotel.address.street}, ${hotel.address.city}, ${hotel.address.province} ${hotel.address.postalCode}, ${hotel.address.country}`}
+        img=""
+      />
     );
+  });
+
+  return (
+    <div className="flex flex-col w-full h-full p-2 overflow-hidden box-border">
+      <Flex
+        justify={"space-between"}
+        align={"center"} /*style={{backgroundColor: 'lightblue'}}*/
+      >
+        <h1>Hotels</h1>
+        <Button type={"default"} size={"large"} href={"/login"}>
+          Login
+        </Button>
+      </Flex>
+      <div className="flex flex-col items-center">
+        <Search locations={locations} />
+      </div>
+
+      <div className="flex flex-row flex-wrap gap-2 overflow-y-auto box-border">
+        {hotelCards}
+      </div>
+    </div>
+  );
 }
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,62 +1,87 @@
-'use server'
-
+"use server";
 
 // import {signIn} from "@/auth";
 
-import {BaseOptionType} from "rc-select/es/Select";
+import { BaseOptionType } from "rc-select/es/Select";
 import { hostname } from "./constants";
 
 export async function authenticate(_currentState: unknown, formData: FormData) {
-    // try {
-    //     await signIn('credentials', formData)
-    // } catch (error) {
-    //     if (error) {
-    //         switch (error.type) {
-    //             case 'CredentialsSignin':
-    //                 return 'Invalid credentials.'
-    //             default:
-    //                 return 'Something went wrong.'
-    //         }
-    //     }
-    //     throw error
-    // }
+  // try {
+  //     await signIn('credentials', formData)
+  // } catch (error) {
+  //     if (error) {
+  //         switch (error.type) {
+  //             case 'CredentialsSignin':
+  //                 return 'Invalid credentials.'
+  //             default:
+  //                 return 'Something went wrong.'
+  //         }
+  //     }
+  //     throw error
+  // }
 }
 
-export default async function getByHotelChain(chainName : string) {
-    const res = await fetch(`http://localhost:8080/hotel/info/byChain/${chainName}`);
-    return res.json()
+export default async function getByHotelChain(chainName: string) {
+  try {
+    const res = await fetch(`${hostname}/hotel/info/byChain/${chainName}`);
+    if (!res.ok) return null;
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
 }
 
 export async function getHotelChains() {
-    const res =  await fetch("http://localhost:8080/hotelChain/info/chainNames")
-    const resJson  = await res.json()
-    const chainNames = [];
+  const chainNames = [];
+
+  try {
+    const res = await fetch(`${hostname}/hotelChain/info/chainNames`);
+    if (!res.ok) return [];
+
+    const resJson = await res.json();
 
     for (let i = 0; i < resJson.length; i++) {
-        chainNames.push({value: resJson[i].chainName})
+      chainNames.push({ value: resJson[i].chainName });
     }
+  } catch (error) {
+    console.error(error);
+  }
 
-    return chainNames
+  return chainNames;
 }
 
 export async function getHotelLocations() {
-    const res = await fetch("http://localhost:8080/hotel/info/cities")
-    const resJson = await res.json()
-    const cities : BaseOptionType[] = [];
+  const cities: BaseOptionType[] = [];
+
+  try {
+    const res = await fetch(`${hostname}/hotel/info/cities`);
+    if (!res.ok) return [];
+
+    const resJson = await res.json();
 
     for (let i = 0; i < resJson.length; i++) {
-        cities.push({value: resJson[i]})
+      cities.push({ value: resJson[i] });
     }
+  } catch (error) {
+    console.error(error);
+  }
 
-    return cities
+  return cities;
 }
 
-export async function bookRoom() {
-
-}
+export async function bookRoom() {}
 
 export async function getAllHotels(): Promise<Hotel[] | undefined> {
-  const response = await fetch(`${hostname}/hotel/info`);
-  const json: Hotel[] | undefined = await response.json();
-  return json;
+  let hotels: Hotel[] = [];
+
+  try {
+    const res = await fetch(`${hostname}/hotel/info`);
+    if (!res.ok) return undefined;
+    hotels = await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return hotels;
 }
