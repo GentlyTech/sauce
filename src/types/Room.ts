8afa89e4d8f47d68289cabbdfa206f61074236b5
GentlyTech <andyyep@hotmail.com>
2024-03-31 21:47:17 -0400

    Remove quotes around keys

--- src/types/Room.ts
+++ src/types/Room.ts
@@ -1,8 +1,8 @@
 interface Room {
-    "hotelId": number,
-    "roomNumber": number,
-    "price": number,
-    "extendable": boolean,
-    "capacity": number,
-    "viewType": string
+  hotelId: number;
+  roomNumber: number;
+  price: number;
+  extendable: boolean;
+  capacity: number;
+  viewType: string;
 }
