989810d0e57f24378b8020b588358a1ad4b65fa0
GentlyTech <andyyep@hotmail.com>
2024-04-02 13:35:45 -0400

    Fix thumbnails styling

--- src/components/custom-card.tsx
+++ src/components/custom-card.tsx
@@ -1,25 +1,34 @@
 import {Card, Rate} from "antd";
 import Image from "next/image";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function CustomCard(props: CardProps) {
-    const imgSrc = props.img != null ? props.img : "";
+  const imgSrc = props.img != null ? props.img : "";
 
-    return (
-        <Card className="flex flex-col w-64">
-            <Image className="w-100" alt="Preview of a hotel or hotel room" fill src={imgSrc}/>
-            <h2 className="w-100">{props.title}</h2>
-            <h3 className="w-100">{props.subtitle}</h3>
-            <Rate disabled defaultValue={props.rating}/>
-            <p className="w-100">{props.body}</p>
-        </Card>
-    );
+  return (
+    <Card
+      className="flex flex-col w-64 overflow-hidden hover:transform-gpu hover:scale-105 transition-transform cursor-pointer"
+      styles={{
+        body: {
+          padding: 0,
+        },
+      }}
+    >
+      <Image className="filter blur-sm brightness-75" alt="Preview of a hotel or hotel room" fill src={imgSrc} />
+      <div className="flex flex-col relative w-full p-6 box-border">
+        <h2 className="w-full">{props.title}</h2>
+        <h3 className="w-full">{props.subtitle}</h3>
+        {props.rating != null ? <Rate disabled defaultValue={props.rating}/> : null}
+        <p className="w-full">{props.body}</p>
+      </div>
+    </Card>
+  );
 }
 
 interface CardProps {
-    title: string;
-    body: string;
-    rating: number
-    subtitle: string;
-    img?: string;
+  title: string;
+  body: string;
+  subtitle: string;
+  rating?: number;
+  img?: string;
 }
